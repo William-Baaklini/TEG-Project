c43c9e1ff0b769ed72f18660498983aefe265928
William-Baaklini <Baaklini.william@gmail.com>
2025-06-06 15:18:13 +0200

    Update vector_store.py

--- app/backend/rag/vector_store.py
+++ app/backend/rag/vector_store.py
@@ -1,5 +1,11 @@
+from dotenv import load_dotenv
+import os
+
 from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
 
 def build_vector_store(chunks, persist_directory="app/backend/rag/chroma_store"):
     embedding_fn = OpenAIEmbeddings(model="text-embedding-3-small")
@@ -11,7 +17,7 @@ def build_vector_store(chunks, persist_directory="app/backend/rag/chroma_store")
     return vectordb
 
 def load_vector_store(persist_directory="app/backend/rag/chroma_store"):
-    embedding_fn = OpenAIEmbeddings(model="text-embedding-3-small")
+    embedding_fn = OpenAIEmbeddings(model="text-embedding-3-small", openai_api_key=api_key)
     return Chroma(
         embedding_function=embedding_fn,
         persist_directory=persist_directory
