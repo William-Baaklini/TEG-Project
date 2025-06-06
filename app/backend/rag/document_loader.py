9983e13a34ef4355cc1e6115305155e3cb31e398
baaklini.william <Baaklini.william@gmail.com>
2025-06-06 02:00:18 +0200

    feat: Updated Rag Retrieval

--- app/backend/rag/document_loader.py
+++ app/backend/rag/document_loader.py
@@ -1,9 +1,17 @@
-from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import TextLoader, PyPDFLoader
 from pathlib import Path
 
-def load_markdown_documents(directory: str):
+def load_documents_from_directory(directory: str):
     all_docs = []
-    for file_path in Path(directory).rglob("*.md"):
-        loader = TextLoader(str(file_path), encoding='utf-8')
+
+    for file_path in Path(directory).rglob("*"):
+        if file_path.suffix == ".md" or file_path.suffix == ".txt":
+            loader = TextLoader(str(file_path), encoding='utf-8')
+        elif file_path.suffix == ".pdf":
+            loader = PyPDFLoader(str(file_path))
+        else:
+            continue  # skip unsupported file types
+
         all_docs.extend(loader.load())
+
     return all_docs
--- app/backend/rag/retriever_wrapper.py
+++ app/backend/rag/retriever_wrapper.py
@@ -1,11 +1,11 @@
-from app.backend.rag.document_loader import load_markdown_documents
+from app.backend.rag.document_loader import load_documents_from_directory
 from app.backend.rag.chunking import chunk_documents
 from app.backend.rag.vector_store import build_vector_store, load_vector_store
 from pathlib import Path
 
 # TODO try to improve the retrieval by using a more sophisticated retriever
 def init_retriever(
-    rag_path="app/rag_data", 
+    rag_path="app/data/rag_data", 
     rebuild=False, 
     search_kwargs=None,
     chunk_size=1000,
@@ -23,7 +23,7 @@ def init_retriever(
 
     if rebuild or not Path(store_path).exists():
         print("🔄 Rebuilding vector store...")
-        docs = load_markdown_documents(rag_path)
+        docs = load_documents_from_directory(rag_path)
         chunks = chunk_documents(docs, chunk_size=chunk_size, chunk_overlap=chunk_overlap)
         vectordb = build_vector_store(chunks, store_path)
     else:
